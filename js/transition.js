6fd3bdcb2bfb9a4c7d7f30e856e3a5aa22cdf7bf
Bruno Carvalho <brunocarvalho287@gmail.com>
2019-05-05 14:08:15 -0300

    Adicionado a transição de opacidade ao final da animação

--- js/transition.js
+++ js/transition.js
@@ -4,11 +4,13 @@ import {TimelineLite} from "gsap";
 class Fade extends Highway.Transition{
     in({from, to, done}){
         const tl = new TimelineLite();
-        tl.fromTo(to, 0.5, {left: "-100%", top:"50%"}, {left: "0%"})
+        tl
+        .fromTo(to, 0.5, {left: "-100%", top:"50%"}, {left: "0%"})
         .fromTo(to, 0.5, {height: "2vh"}, {height: "90vh", top: "10vh", onComplete(){
             from.remove();
             done();
         }})
+        .fromTo(to.children[0], 1, {opacity: 0}, {opacity: 1})
     }
 
     out({done}){
